67b8de58f416a80f7642349cdc72658f3d8f2f03
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2018-01-04 18:55:33 +0100

    completed map/reduce exercises

--- map-filter/js/map_reduce_tests.js
+++ map-filter/js/map_reduce_tests.js
@@ -64,5 +64,106 @@ describe('using map and/or filter', function () {
 });
 
 describe('using reduce', () => {
-    it('');
+    it('calculate the sum of all elements inside an array', () => {
+
+        function sum(array) {
+            return 0;
+        }
+
+        expect(sum([1, 2, 3])).to.be.eql(6);
+
+        // try something like [1,2,3].reduce(.. , ..)
+        expect([1, 2, 3]).to.be.eql(2);
+
+    });
+
+    it('calculate the average of all elements inside an array', () => {
+
+        function average(array) {
+            return 0;
+        }
+
+        expect(average([1, 2, 3])).to.be.eql(2);
+
+        // try something like [1,2,3].reduce(.. , ..)
+        expect([1, 2, 3]).to.be.eql(2);
+
+    });
+
+    it('verify that all elements of an array satisfy a given condition (same as Array.prototype.every)', () => {
+
+        function isAllEven(array) {
+            return true;
+        }
+
+        expect(isAllEven([2, 4, 6])).to.be.true;
+
+        // try something like [2,4,6].reduce(.. , ..)
+        expect([2, 4, 6]).to.be.true;
+
+        // you can obtain this also with Array.prototype.every
+        expect([2, 4, 6].every(/* ... */)).to.be.true;
+    });
+
+    it('verify that at least one element of an array satisfies a given condition (same as Array.prototype.some)', () => {
+
+        function atLeastOneEven(array) {
+            return true;
+        }
+
+        expect(atLeastOneEven([1, 4, 7])).to.be.true;
+
+        // try something like [1,4,7].reduce(.. , ..)
+        expect([1, 4, 7]).to.be.true;
+
+        // you can obtain this also with Array.prototype.some
+        expect([2, 4, 6].some(/* ... */)).to.be.true;
+    });
+
+    it('clone an object while modifying it partially', () => {
+
+        function makeEvenValuesOneven(obj) {
+            return obj;
+        }
+
+        expect(makeEvenValuesOneven({a: 2, b: 5})).to.be.eql({a: 3, b: 5});
+
+        // try something like Object.keys(obj).reduce(.. , ..)
+        expect({a: 2, b: 5}).to.be.eql({a: 3, b: 5});
+    });
+
+    describe('do something n times by defining an \'enumeration\' factory', () => {
+
+        function enumeration(n) {
+            return Array.from(Array(n).keys());
+        }
+
+        it('verifying that it works', () => {
+            expect(enumeration(1)).to.be.eql([0]);
+            expect(enumeration(5)).to.be.eql([0, 1, 2, 3, 4]);
+        });
+
+        it('and using it to command a reduction', () => {
+
+            // you may find the following regex useful: /^(.*)\.[^\.]+$/
+            // or you just may go down on splitting the string
+            function chopNTimes(times, string) {
+                return string;
+            }
+
+            expect(chopNTimes(3, 'ab.cde.f.gh.ijk.lm')).to.be.eql('ab.cde.f');
+
+            // where do you put the 'ab.cde.f.gh.ijk.lm' string?
+            expect(enumeration(/*times*/).reduce(/*.. , ..*/)).to.be.eql('a.b.c.d.e.f');
+        });
+
+        it('in a working proof of concept', () => {
+
+            const THE_REGEX = /^(.*)\.[^\.]+$/;
+
+            expect(enumeration(3)
+                .reduce((acc, curr) => THE_REGEX.exec(acc)[1], 'ab.cde.f.gh.ijk.lm'))
+                .to.be.eql('ab.cde.f');
+        });
+    });
 });
