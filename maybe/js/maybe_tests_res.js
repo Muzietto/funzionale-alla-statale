284ee63955d0be031f2bc56cc5cd1e550fa3a817
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-09-26 09:14:41 +0200

    nicer tests and partial results

--- maybe/js/maybe_tests_res.js
+++ maybe/js/maybe_tests_res.js
@@ -72,7 +72,15 @@ describe('maybes are optional values', function () {
                     .to.be.eql(Maybe.Just([1, 2]));
 
                 function maybeList1(listOfMaybes) {
-                    return listOfMaybes.reduce(/* implement me */).fmap(/* why would you need me? */);
+                    return listOfMaybes.reduce((acc, currMaybe) => {
+                        if (currMaybe.isJust) {
+                            return MaybeCons
+                                .apply(currMaybe)
+                                .apply(acc);
+                        } else {
+                            return acc;
+                        }
+                    }, Maybe.Just([])).fmap(reverseArray);
                 }
             });
             it('that invalidates the whole list once a Nothing is encountered', function () {
@@ -88,7 +96,17 @@ describe('maybes are optional values', function () {
                 expect(maybeList2([Maybe.Just(1), Maybe.Just(2)]))
                     .to.be.eql(Maybe.Just([1, 2]));
 
-                function maybeList2(listOfMaybes) {/* implement me */}
+                function maybeList2(listOfMaybes) {
+                    return listOfMaybes.reduce((acc, currMaybe) => {
+                        if (currMaybe.isJust && acc.isJust) {
+                            return MaybeCons
+                                .apply(currMaybe)
+                                .apply(acc);
+                        } else {
+                            return Maybe.Nothing();
+                        }
+                    }, Maybe.Just([])).fmap(reverseArray);
+                }
             });
         });
     });
@@ -99,9 +117,7 @@ describe('maybes are optional values', function () {
 
             // as long as the account balance is positive or zero, its value is Just(balance)
             // if the account balance becomes negative, its value is Nothing
-            var changeBalance = delta => balance => (balance + delta >= 0)
-              ? Maybe.of(balance + delta)
-              : Maybe.Nothing();
+            var changeBalance = delta => balance => (balance + delta >= 0) ? Maybe.of(balance + delta) : Maybe.Nothing();
 
             expect(Maybe.of(2).bind(changeBalance(1)).bind(changeBalance(-3)).getOrElse('you are broke'))
                 .to.be.eql(0);
@@ -111,14 +127,11 @@ describe('maybes are optional values', function () {
 
         it('with a flexible overdraft value', () => {
 
-            var changeBalance = delta => balance => (balance + delta >= 0)
-              ? Maybe.of(balance + delta)
-              : Maybe.Nothing();
+            var changeBalance = delta => balance => (balance + delta >= 0) ? Maybe.of(balance + delta) : Maybe.Nothing();
             // this function has one drawback: the maximum overdraft value (0) is hardcoded inside, and not modifiable
             // what we need is to pass around a Maybe.Just(Tuple.Pair(balance, overdraft))
 
             var changeBalance = delta => pair => pair; // TODO: implement me
-
             expect(Maybe.of(Tuple.Pair(2, -1)).bind(changeBalance(-1)).getOrElse('you exceeded your overdraft'))
                 .to.be.eql(Tuple.Pair(1, -1));
             expect(Maybe.of(Tuple.Pair(2, -1)).bind(changeBalance(-3)).getOrElse('you exceeded your overdraft'))
@@ -136,29 +149,20 @@ describe('maybes are optional values', function () {
         });
 
         it('but the possibility to recover from debt is tricky', () => {
+            var changeBalance = delta => balance => (balance + delta >= 0) ? Maybe.of(balance + delta) : Maybe.Nothing();
+            // once we are in red and the balance becomes a Nothing, every function that we bind will actually not operate
 
-            var changeBalance = delta => balance => (balance + delta >= 0)
-              ? Maybe.of(balance + delta)
-              : Maybe.Nothing();
-            // once we are in red and the balance becomes a Nothing,
-            // every function that we bind will actually not operate
+            // orElse to the rescue!
+            expect(Maybe.of(2).bind(changeBalance(-3)).orElse(/* TODO: implement me */).getOrElse('you are broke'))
+                .to.be.eql(0);
 
-            // use orElse to rescue the situation!
-            expect(Maybe.of(2)
-              .bind(changeBalance(-3))
-              .orElse(/* TODO: implement me */)
-              .getOrElse('you are broke')).to.be.eql(0);
-
-            expect(Maybe.of(2)
-              .bind(changeBalance(-3))
-              .bind(changeBalance(-4))
-              .orElse(/* TODO: implement me */)
-              .bind(changeBalance(5))
-              .getOrElse('you are broke')).to.be.eql(5);
+            expect(Maybe.of(2).bind(changeBalance(-3)).bind(changeBalance(-4)).orElse(/* TODO: implement me */).bind(changeBalance(5)).getOrElse('you are broke'))
+                .to.be.eql(5);
             // question: what is the value of balance when orElse is applied in the last test?
         });
     });
-});
+})
+;
 
 function parsedIntOrNull(str) {
     result = null;
